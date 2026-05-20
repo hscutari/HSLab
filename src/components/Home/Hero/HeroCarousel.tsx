a962663f3b3ae52cdaee8fed135d279951743f5f
hcutari <henrique.scutari@eidasolutions,com>
2026-05-20 13:07:41 -0300

    changing first image

--- src/components/Home/Hero/HeroCarousel.tsx
+++ src/components/Home/Hero/HeroCarousel.tsx
@@ -107,6 +107,44 @@ const HeroCarousal = () => {
           </div>
         </div>
       </SwiperSlide>
+      <SwiperSlide>
+        <div className="flex items-center pt-6 sm:pt-0 flex-col-reverse sm:flex-row">
+          <div className="max-w-[394px] py-10 sm:py-15 lg:py-26 pl-4 sm:pl-7.5 lg:pl-12.5">
+            <div className="flex items-center gap-4 mb-7.5 sm:mb-10">
+              <span className="block font-semibold text-heading-3 sm:text-heading-1 text-blue">
+                60%
+              </span>
+              <span className="block text-dark text-sm sm:text-custom-1 sm:leading-[24px]">
+                {t("sale")}
+                <br />
+                {t("off")}
+              </span>
+            </div>
+
+            <h1 className="font-semibold text-dark text-xl sm:text-3xl mb-3">
+              <a href="/shop-details">Esse eh um novo teste de desconto</a>
+            </h1>
+
+            <p>Olha o anhanha.</p>
+
+            <a
+              href="/shop-details"
+              className="inline-flex font-medium text-white text-custom-sm rounded-md bg-dark py-3 px-9 ease-out duration-200 hover:bg-blue mt-10"
+            >
+              {t("shopNow")}
+            </a>
+          </div>
+
+          <div>
+            <Image
+              src="/images/hero/hero-01.png"
+              alt="product"
+              width={351}
+              height={358}
+            />
+          </div>
+        </div>
+      </SwiperSlide>
     </Swiper>
   );
 };
